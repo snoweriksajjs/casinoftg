859b5df992039249765be82036334c1302812585
snoweriksajjs <snowerik10@gmail.com>
2025-06-03 03:04:06 +0500

    Update casinoftg.py

--- casinoftg.py
+++ casinoftg.py
@@ -15,13 +15,14 @@ class ReplyMod(loader.Module):
     }
 
     def __init__(self):
-        self.config = {"enabled": False, "message": None}
-
-    async def client_ready(self, client, db):
-        self.client = client
-        self.db = db
-        self.config["enabled"] = db.get(self.name, "enabled", False)
-        self.config["message"] = db.get(self.name, "message", None)
+        self.config = loader.ModuleConfig(
+            loader.ConfigValue(
+                "enabled", False, lambda: "Включить автоответ"
+            ),
+            loader.ConfigValue(
+                "message", "", lambda: "Текст автоответа"
+            )
+        )
 
     @loader.unrestricted
     async def replycmd(self, message):
@@ -30,21 +31,23 @@ class ReplyMod(loader.Module):
             return await message.edit(self.strings["usage"])
 
         cmd, *text = args.split(" ", 1)
+        cmd = cmd.lower()
+
         if cmd == "on":
             if not self.config["message"]:
                 return await message.edit(self.strings["no_message"])
             self.config["enabled"] = True
-            self.db.set(self.name, "enabled", True)
             return await message.edit(self.strings["enabled"])
+
         elif cmd == "off":
             self.config["enabled"] = False
-            self.db.set(self.name, "enabled", False)
             return await message.edit(self.strings["disabled"])
+
         elif cmd == "set" and text:
             msg = text[0]
             self.config["message"] = msg
-            self.db.set(self.name, "message", msg)
             return await message.edit(self.strings["set_message"].format(msg))
+
         else:
             return await message.edit(self.strings["usage"])
 
@@ -53,7 +56,9 @@ class ReplyMod(loader.Module):
             return
         try:
             await message.react("💤")
-        except: pass
+        except:
+            pass
         try:
             await message.reply(self.config["message"])
-        except: pass
+        except:
+            pass
